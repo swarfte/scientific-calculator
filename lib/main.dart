5a991dc5d96a92bddcb521515b3000dca5329789
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2026-07-20 17:49:00 +0800

    upload file correctly by .gitignore

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:scientific_calculator/pages/home.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scientific Calculator',
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
+  }
+}
